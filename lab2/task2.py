70585af3e964d6350afe16471a8604892a650e62
Verner-Ad <covox.simulation@gmail.com>
2024-03-23 17:54:26 +0300

    task2

--- lab2/task2.py
+++ lab2/task2.py
@@ -3,5 +3,18 @@ for i in range(int(input())):
     inp.append(input().split())
 cust = []
 for i in inp:
-    if i not in cust:
-        cust.append(i)
+    if i[0] not in cust:
+        cust.append(i[0])
+custDict = {}
+for i in cust:
+    tempDict = {}
+    for j in inp:
+        if i == j[0]:
+            if j[1] in list(tempDict.keys()):
+                tempDict[j[1]] += j[2]
+            else:
+                tempDict[j[1]] = j[2]
+    custDict[i] = tempDict
+CL = list(custDict.keys())
+for i in CL:
+    print(i + ":" + custDict[i])
